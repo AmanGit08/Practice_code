a6e7955a27fc37e269223cb9d42955aab415d13c
AmanGit08 <amanprajapati5545@gmail.com>
2026-02-19 07:44:38 +0530

    chapter1 to 3 addd with two files in it lets see

---
+++ chapter 1 to 3/add .py
@@ -0,0 +1,4 @@
+# write the problem to add no 
+a = 10 
+b = 20
+print(a + b)
---
+++ chapter 1 to 3/escape_seq.py
@@ -0,0 +1,19 @@
+# some escape sequence 
+a = "Hello,\nWorld!"
+print(a)
+b = "Hello,\tWorld!"
+print(b)
+c = "Hello,\\World!"
+print(c)
+# This script demonstrates the use of escape sequences in Python strings.
+# give some more examples
+d = "He said, \"Hello!\""
+print(d)
+e = 'It\'s a beautiful day!'
+print(e)
+f = "C:\\Users\\Aman\\Documents"
+print(f)
+g = "First Line\rSecond Line"
+print(g)
+h = "Column1\bColumn2"
+print(h)
