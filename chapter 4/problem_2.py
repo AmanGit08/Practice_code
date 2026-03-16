2472de19560680ca45c55f3013cc547ea1a504a5
AmanGit08 <amanprajapati5545@gmail.com>
2026-03-16 21:54:31 +0530

    chapter4

---
+++ chapter 4/problem_2.py
@@ -0,0 +1,18 @@
+marks = []
+
+f1 = input("Enter the name of amarks: ")
+marks.append(f1)
+f2 = input("Enter the name of amarks: ")
+marks.append(f2)
+f3 = input("Enter the name of amarks: ")
+marks.append(f3)
+f4 = input("Enter the name of amarks: ")
+marks.append(f4)
+f5 = input("Enter the name of amarks: ")
+marks.append(f5)
+f6 = input("Enter the name of amarks: ")
+marks.append(f6)
+f7 = input("Enter the name of amarks: ")
+marks.append(f7)
+marks.sort
+print(marks)
